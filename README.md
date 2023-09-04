95c8e144ec19a5cf53b764b3e063738d541cc4f3
zxn <zxn288912@163.com>
2023-09-04 15:22:22 +0800

    feat: add some functions

---
+++ README.md
@@ -0,0 +1,14 @@
+# Achieve-proxy
+
+This is a simplified library for implementing proxy functionality.
+
+# Installing
+```js
+npm i -D @zxn2889/achieve-proxy
+```
+
+or (recommend)
+
+```js
+pnpm add -D @zxn2889/achieve-proxy
+```
--- rollup.config.base.js
+++ rollup.config.base.js
@@ -3,7 +3,7 @@ import pkg from './package.json'
 import json from '@rollup/plugin-json'
 import { nodeResolve } from '@rollup/plugin-node-resolve'
 import commonjs from '@rollup/plugin-commonjs'
-// import eslint from '@rollup/plugin-eslint'
+import eslint from '@rollup/plugin-eslint'
 import { babel } from '@rollup/plugin-babel'
 
 const formatName = 'achieve'
@@ -30,7 +30,7 @@ export default {
         commonjs({ include: /node_modules/ }),
         nodeResolve({ preferBuitins: true, main: true, brower: true }),
         typescript(),
-        // eslint(),
+        eslint(),
         babel({ exclude: 'node_modules/**' })
     ]
 }
